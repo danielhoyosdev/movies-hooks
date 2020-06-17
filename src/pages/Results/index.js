a78317457792ed001c3a4b32db12b4fb6f8be89a
Daniel Hoyos <danielhoyos350@gmail.com>
2020-06-16 23:04:14 -0500

    Haciendo Fetch a la API y obteniendo los datos con useSelector en la page de Results

--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,11 +1,24 @@
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Container } from '@material-ui/core';
 import queryString from 'query-string';
 
+// Redux
+import { searchMovie } from '../../redux/actions/search';
+import { movieResults, isSearchLoading } from '../../redux/selectors';
+
 export default ({ location }) => {
+    const dispatch = useDispatch();
+    const movies = useSelector(state => movieResults(state));
+
+    console.log(movies);
 
     useEffect(() => {
         const { movieName } = queryString.parse(location.search);
+
+        if(movieName && !movies) {
+            dispatch(searchMovie({ movieName }));
+        }
     });
 
     return (
